a3c7d693b6dc12152fc6978b949c23783b8803df
Matija Jovanovic <jovanovic.m04@htlwienwest.at>
2022-01-22 17:37:42 +0100

    List Group onChange funktion eingebaut, bissi List Group geändert und so es funktioniert jetzt so

--- Starter/wwwroot/js/site.js
+++ Starter/wwwroot/js/site.js
@@ -2,3 +2,63 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+//var items = document.getElementsByClassName("list-group-item active");
+
+
+//var items = document.getelementsbyclassname("list-group-item");
+//for (var i = 0; i < items.length; i++) {
+//    if ((' ' + items[i].classname + ' ').indexof(' active ') > -1)
+//        items[i].setattribute('style', 'color: #ffff00');
+//    else
+//        items[i].setattribute('style', 'color: #ccc');
+//}
+
+
+//tb.innerHTML = items.innerHTML;
+
+
+
+function bruh() {
+    if (select.value == "1") {
+        label.value = "op 1";
+    }
+    else if (select.value == "2") {
+        label.value = "op 2";
+    }
+    else if (select.value == "3") {
+        label.value = "op 3";
+    }
+    else if (select.value == "4") {
+        label.value = "op 4";
+    }
+
+};
+
+var label = document.getElementById("exampleFormControlTextarea1");
+
+
+
+
+$('.list-group-item').on('click', function () {
+    var $this = $(this);
+    var $alias = $this.data('alias');
+
+    $('.active').removeClass('active');
+    $this.toggleClass('active')
+
+    // Pass clicked link element to another function
+    myfunction($this, $alias)
+})
+
+
+
+function myfunction($this, $alias) {
+    console.log($this.text());  // Will log Paris | France | etc...
+
+    console.log($alias);  // Will output whatever is in data-alias=""
+
+    document.getElementById("exampleFormControlTextarea1").value = $this.text();
+}
